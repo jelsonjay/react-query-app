c10069588ac35b0439bcd2413ae91aec78f92970
Jelson J <jelson20@outlook.com>
2021-02-18 15:26:01 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Reacr, {useState} from 'react'
 import './App.css';
+import Navbar from './components/Navbar';
+import Planets from './components/Planet';
+import Person from './components/';
 
 function App() {
+const [item, setItems] = useState('planets')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1>Hello World!</h1>
+   <Navbar setItems={setItems}/>
+   <di className='content'>
+  {item === 'planets' ? <Planets /> : <Person />}
+   </di>
     </div>
   );
 }
